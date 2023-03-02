6a731660565aa9af88bb3917766fbe8a5117c5fe
idanv87 <idanv@campus.technion.ac.il>
2023-03-02 16:31:24 +0200

    first

--- wave1d.py
+++ wave1d.py
@@ -30,7 +30,7 @@ class Calc:
         self.U = U
         self.D=D
         self.f=f
-        self.rho=Rho
+        self.Rho=Rho
         self.Psi=Psi
         self.p=p
 
@@ -99,12 +99,18 @@ rho=[1]
 p=np.random.rand(N1,N2)
 f=np.random.rand(N2,1)
 
-def exp_max(p, u,D,alpha,rho,psi,num_iter=2):
+def exp_max(p, u,D,alpha,rho,psi,num_iter=4):
+    assert len(rho)==len(D)
+    assert len(alpha)==len(psi)
+
     for _ in range(num_iter):
         f=sum([alpha[i]*psi[i] for i in range(len(psi))])
         C=Calc(p, u,D,f,rho,psi)
         alpha=C.solve()
-        rho=C.rho
+        rho=C.up_rho()
+        print(rho)
+  
+
 
     return alpha, rho
 
@@ -130,80 +136,16 @@ def create_D2(n):
     D2=csr_matrix(D2)
     return D2
 
-
-
-
-def solve_wave(f0,f1,Dxx,Dyy,cfl,m):
-   for t in range(m):
-       f=2*f1-f0+cfl*(Dxx+Dyy)@f1
-       f0=f1
-       f1=f
-
-   return f   
-
-class wave_data:
-    cfl=0.4
-
-    def __init__(self,N_x,N_t):
-        self.N_x=N_x
-        self.N_t=N_t 
-        self.create_mtx(self.N_x)
-        
-
-
-
-    # @classmethod
-    def create_grid_and_f0(self):
-        x=np.linspace(0,1,self.N_x+1)[1:]
-        dx=x[1]-x[0]
-        dt=np.sqrt(self.cfl)*dx
-
-        f0=np.sin(2*math.pi*x)
-        # *np.cos(math.pi*t)
-        # f0[int(self.N_x/2):int(self.N_x/2)+2,int(self.N_x/2):int(self.N_x/2)+2]=1
-        f1=f0*np.cos(2*math.pi*dt)
-        return f0,f1
-    
-    @classmethod
-    def create_mtx(self,n): 
-        self.Dxx=create_D2(n)
-        
-        
-   
-    def solve_equation(self,m):
-        f0=self.create_grid_and_f0()[0]
-        f1=self.create_grid_and_f0()[1]
-        sol=[]
-        for t in range(m):
-            sol.append(f0)
-            f=2*f1-f0+wave_data.cfl*(self.Dxx)@f1
-            f0=f1
-            f1=f
-   
-        return sol    
-              
-        
-
-
-        # plt.plot(f0[2,:])
-        # plt.show()
 N=30
-C=wave_data(N,20)
-sol=C.solve_equation(100)
-
-x=np.linspace(0,1,N+1)[1:]
-dx=x[1]-x[0]
-dt=np.sqrt(C.cfl)*dx
-
-# f=np.sin(2*math.pi*x)*np.cos(2*math.pi*4*dt)
-# print(np.linalg.norm(sol[4]-f))
-
+N_samples=5
 def create_A(dt,h,n):
     D=create_D2(n).toarray()/h**2
     A=np.block([[np.zeros((n,n)),np.eye(n)],[D,np.zeros((n,n))]])
     return np.eye(2*n)+dt*A
+p=np.zeros((N_samples,2*N))
+p[:N_samples, :N_samples]=np.eye(N_samples)
 
-N=30
+
 x=np.linspace(0,1,N+1)[1:]
 dx=x[1]-x[0]
 dt=np.sqrt(0.1)*dx
@@ -212,9 +154,16 @@ g=f*0
 f0=np.vstack([f,g])
 
 mtx=create_A(dt,dx,N)
-for _ in range(10):
+psi=[np.vstack([f,g]),0.5*np.vstack([np.sin(4*math.pi*x).reshape(N,1),g])] # base functions 
+alpha=[0.1,0.01]
+rho=[0.4,0.4,0.2]
+u=[]
+D=[mtx,mtx@mtx, mtx@mtx@mtx]
+for i in range(10):
     f0=mtx@f0
+    if i>7:
+        u.append(p@f0)
 
-plt.plot(f0[:N])
-plt.show()   
 
+a,b=exp_max(p, u,D,alpha,rho,psi)
+print(a)
